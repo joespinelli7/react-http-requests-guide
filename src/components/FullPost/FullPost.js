a8c317d61d554c618d77c94c4f440a29ae4874ef
github username <josephspinelli@yahoo.com>
2019-06-12 14:49:48 -0400

    body of post

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -44,7 +44,7 @@ class FullPost extends Component {
           post = (
             <div className="FullPost">
             <h1>{this.state.loadedPost.title}</h1>
-            <p>{this.state.body}</p>
+            <p>{this.state.loadedPost.body}</p>
             <div className="Edit">
             <button onClick={this.deletePostHandler} className="Delete">Delete</button>
             </div>
