b067caa231a483a910dca0cdd3f0197d43c88308
github username <josephspinelli@yahoo.com>
2019-06-23 14:54:03 -0400

    setting up react routing and rearranging file structure

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
+import { BrowserRouter } from 'react-router-dom';
+// b/c wrapping the div in App.js component with react-router-dom, now every sub component has
+// access to routing.
 
 import Blog from './containers/Blog/Blog';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Blog />
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Blog />
+        </div>
+      </BrowserRouter>
     );
   }
 }
