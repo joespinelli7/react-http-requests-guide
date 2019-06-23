05148cd752af2c3b1ddef3d46d7e4188a01dfc97
github username <josephspinelli@yahoo.com>
2019-06-23 15:57:13 -0400

    restructuring components and files for routing

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,60 +1,9 @@
 import React, { Component } from 'react';
-// import axios from 'axios';
-import axios from '../../axios';
 import './Blog.css';
+import Posts from './Posts/Posts';
 
 class Blog extends Component {
-    state = {
-      posts: [],
-      selectedPostId: null,
-      error: false
-    }
-
-    componentDidMount() {
-      axios.get('/posts')
-        .then(res => {
-          const posts = res.data.slice(0, 4);
-          const updatedPosts = posts.map(post => {
-            return {
-              ...post,
-              author: 'Joe'
-            }
-          })
-          this.setState({
-            posts: updatedPosts
-            // console.log(res)
-          });
-        })
-        .catch(error => {
-          // console.log(error)
-          this.setState({
-            error: true
-          })
-        });
-    }
-
-    postSelectedHandler = (id) => {
-      this.setState({
-        selectedPostId: id
-      });
-    }
-
-    // using catch in line 30 to catch any errors and if we do render line 46 b/c we're getting an error
-    // trying to make the GET request. However, if there is no error in GET request then we render all
-    // posts smoothly.
     render () {
-        let posts = <p style={{textAlign: 'center'}}>Something went wrong :(</p>;
-        if (!this.state.error) {
-          posts = this.state.posts.map(post => {
-            return <Post
-                      key={post.id}
-                      title={post.title}
-                      author={post.author}
-                      clicked={() => this.postSelectedHandler(post.id)}
-                   />
-          });
-        }
-
         return (
             <div className="Blog">
                 <header>
@@ -65,9 +14,7 @@ class Blog extends Component {
                     </ul>
                   </nav>
                 </header>
-                <section className="Posts">
-                    {posts}
-                </section>
+                <Posts />
             </div>
         );
     }
---
+++ src/containers/Blog/Posts/Posts.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import axios from '../../../axios';
+import Post from '../../../components/Post/Post';
+import './Posts.css';
+
+class Posts extends React.Component {
+  state = {
+    posts: []
+  }
+
+  // using catch in line 26 to catch any errors and if we do render line 41 b/c we're getting an error
+  // trying to make the GET request. However, if there is no error in GET request then we render all
+  // posts smoothly.
+  componentDidMount() {
+    axios.get('/posts')
+      .then(res => {
+        const posts = res.data.slice(0, 4);
+        const updatedPosts = posts.map(post => {
+          return {
+            ...post,
+            author: 'Joe'
+          }
+        })
+        this.setState({
+          posts: updatedPosts
+          // console.log(res)
+        });
+      })
+      .catch(error => {
+        console.log(error)
+        // this.setState({
+        //   error: true
+        // })
+      });
+  }
+
+  postSelectedHandler = (id) => {
+    this.setState({
+      selectedPostId: id
+    });
+  }
+
+  render() {
+    let posts = <p style={{textAlign: 'center'}}>Something went wrong :(</p>;
+    if (!this.state.error) {
+      posts = this.state.posts.map(post => {
+        return <Post
+                  key={post.id}
+                  title={post.title}
+                  author={post.author}
+                  clicked={() => this.postSelectedHandler(post.id)}
+               />
+      });
+    }
+
+    return (
+      <section className="Posts">
+          {posts}
+      </section>
+    )
+  }
+}
+
+export default Posts;
